86ab04ef9f660898aa2e6f78ed51f3bc017fd5f8
ASgmbv <alimbeksagymbaev@gmail.com>
2021-04-01 14:29:12 +0600

    added support page

---
+++ pages/support.js
@@ -0,0 +1,17 @@
+import Header from "../components/Header";
+import { Container, Heading } from "@chakra-ui/react";
+
+const SupportPage = (params) => {
+  return (
+    <>
+      <Header />
+      <Container maxW="7xl">
+        <Heading size="md" py="50px">
+          Coming soon ...
+        </Heading>
+      </Container>
+    </>
+  );
+};
+
+export default SupportPage;
